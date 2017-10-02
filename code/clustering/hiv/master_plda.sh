e1b97cfc23909be6d4a5798c67960ee29f032338
Ehsan Sadrfaridpour <esadrfa@g.clemson.edu>
2017-10-02 02:27:07 -0400

    cancer updates

---
+++ code/clustering/hiv/master_plda.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+user=`whoami`
+hypo_name="hiv"
+source_path="/home/"$user"/alg/MOLIERE/code/clustering/scripts/" 
+data_path="/scratch2/"$user"/moliere/"$hypo_name"/out/" 
+output_path="/home/"$user"/alg/MOLIERE/results/"$hypo_name"/"
+out_model="/scratch2/"$user"/plda/model/"$hypo_name"/"
+for ds in `ls $data_path`;
+do
+     exp_info="moliere_plda"$hypo_name"_"$ds"_"`/bin/date +%m%d%y_%H%M`;
+     echo $exp_info  >> "./running_processes";
+     echo $ds ;
+     qsub -v user=$user,src_path=$source_path,dataset_name=$ds,model_out=$out_model"_"$ds,output_path=$output_path,experiment_info=$ds,data_path=$data_path worker_plda.pbs
+     #echo "qsub -v user=$user,src_path=$source_path,dataset_name=$ds,model_out=$out_model"_"$ds,output_path=$output_path,experiment_info=$ds,data_path=$data_path worker_plda.pbs"
+     #exit #this is only for test to run the whole thing once only
+done
---
+++ code/clustering/scripts/runDijk2Data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+module load python
+module load sqlite
+
+hypo_name="hiv"
+input_path="/scratch2/esadrfa/moliere/"$hypo_name
+rm -rf $input_path"/out"
+mkdir $input_path"/out"
+
+#cp /zfs/safrolab/users/jsybran/moliere/data/network/final.labels .
+#cp /zfs/safrolab/users/jsybran/moliere/data/processedText/abstracts.txt .
+
+#./dijk2Data.py -l ./final.labels  -p ./dijk2Data -o hiv/out/ -a ./abstracts.txt #-v
+#./dijk2Data.py -l ./final.labels  -p ./test -o hiv/out/ -a ./abstracts.txt -v
+
+#the main one 
+./dijk2Data.py -l $input_path"/final.labels" -p $input_path"/clustered_clouds.txt" -o $input_path"/out/" -a $input_path"/abstracts.txt"
+
+#only for HIV at Oct 1, 2017
+#./dijk2Data.py -l $input_path"/final.labels" -p $input_path"/hiv_clustered_clouds.txt_old" -o $input_path"/out/" -a $input_path"/abstracts.txt"
+
