b3f6a7e3e9bb9708d3ead8c631bd4dbb7b844253
JSybrandt <nalta45@gmail.com>
2018-05-03 16:11:02 -0400

    getPapers now writes a variable number of pmids (not labels, just the pmid #) per topic. Files are cleaned now that it doesn't rely on the network

--- run_query/src/getPapers/main.cpp
+++ run_query/src/getPapers/main.cpp
@@ -29,24 +29,26 @@
 
 using namespace std;
 
+string label2pmid(const string& label){
+  if(label.substr(0, 4) != "PMID")
+    throw runtime_error("Attempted to convert non-pmid label to pmid");
+  size_t underscoreIdx = label.find('_');
+  return label.substr(4, underscoreIdx-4);
+}
+
+
 bool verbose;
 #define vout if(::verbose) cout
 
 int main(int argc, char ** argv){
+
   cmdline::parser p;
 
   p.add<string>("cloud", 'c', "Cloud file", true);
   p.add<string>("inference", 'i', "Inference file", true);
-  // p.add<string>("abstracts", 'a', "Abstract file", true);
   p.add<string>("labels", 'l', "Label file", true);
-  p.add<string>("graph", 'g', "Edges file", true);
   p.add<string>("output", 'o', "Path to resulting file", true);
   p.add<string>("topic-model", 'm', "Topic model from VIEW_FILES", true);
-  p.add<string>("ngram-vecs", 'N', "ngram vector file", false, "");
-  p.add<string>("pmid-vecs", 'P', "pmid vector file", false, "");
-  p.add<string>("umls-vecs", 'U', "cuid vector file", false, "");
-  p.add<string>("source-label", 's', "Source Label", true);
-  p.add<string>("target-label", 't', "Target Label", true);
   p.add("verbose", 'v', "Output debug info.");
   p.add<size_t>("papers-per-topic", 'X', "Num. pmid per topic", false, 5);
 
@@ -55,16 +57,9 @@ int main(int argc, char ** argv){
 
   string cloudPath = p.get<string>("cloud");
   string inferencePath = p.get<string>("inference");
-  // string abstractPath = p.get<string>("abstracts");
-  string graphPath =  p.get<string>("graph");
   string labelPath =  p.get<string>("labels");
   string outPath = p.get<string>("output");
   string topicModelPath = p.get<string>("topic-model");
-  string ngramVecsPath = p.get<string>("ngram-vecs");
-  string pmidVecsPath = p.get<string>("pmid-vecs");
-  string umlsVecsPath = p.get<string>("umls-vecs");
-  string sourceLabel = p.get<string>("source-label");
-  string targetLabel = p.get<string>("target-label");
   ::verbose = p.exist("verbose");
   size_t papersPerTopic = p.get<size_t>("papers-per-topic");
 
@@ -76,61 +71,6 @@ int main(int argc, char ** argv){
   vector<Topic> topicModel = getTM(topicModelPath);
   vout << "  -- Done --" << endl;
 
-  vout << "Getting all unique wordsInTopicModel";
-  unordered_set<string> wordsInTopicModel;
-  wordsInTopicModel.insert(sourceLabel);
-  wordsInTopicModel.insert(targetLabel);
-  for(const Topic & topic : topicModel){
-    for(const auto & p : topic){
-      wordsInTopicModel.insert(p.first);
-    }
-  }
-  vout << "  -- Done --" << endl;
-
-  //vout << "Getting Necessary Vectors";
-  //VectorManager vectors;
-  //vectors.registerTag(ngramVecsPath);
-  //vectors.registerTag(pmidVecsPath, 'P');
-  //vectors.registerTag(umlsVecsPath, 'C');
-  //unordered_map<string, vector<float>> word2vec = vectors.getVectors(wordsInTopicModel);
-  //vout << "  -- Done --" << endl;
-
-  //vout << "Getting Topic Centroids";
-  //vector<vector<float>> topicCentroids;
-  //topicCentroids.reserve(topicModel.size());
-  //for(const Topic& t: topicModel){
-    //topicCentroids.push_back(getCentroid(t, word2vec));
-  //}
-  //vout << "  -- Done --" << endl;
-
-  //pair<string, string> queryWords = {sourceLabel, targetLabel};
-
-  //vout << "Checking query word vectors exist. Making estimates if not";
-  //if(word2vec.find(sourceLabel) == word2vec.end()){
-    //vout << " MISSING: " << sourceLabel;
-    //word2vec[sourceLabel] = estimateVector(sourceLabel, graphPath, vectors, labels);
-  //}
-  //if(word2vec.find(targetLabel) == word2vec.end()){
-    //vout << " MISSING: " << targetLabel;
-    //word2vec[targetLabel] = estimateVector(targetLabel, graphPath, vectors, labels);
-  //}
-  //vout << "  -- Done --" << endl;
-
-  //vout << "Creating NN Net";
-  //// NOTE: query words are idx 0 and 1, 2-n are the topics (0-(n-2))
-  //vector<nodeIdx> topicPath = simpleTopicNetData(queryWords,
-                                                 //topicModel,
-                                                 //topicCentroids,
-                                                 //word2vec).second;
-  //vout << "  -- Done --" << endl;
-
-  //vout << "Getting best topics from L2";
-  //BestCentrL2Metric topicMet(queryWords, topicModel, topicCentroids, word2vec);
-  //vout << "  -- Done --" << endl;
-
-  // list of topic idx to score, sorted.
-  //vector<pair<size_t, float>> bestTopics = topicMet.getBestTopics();
-
   vout << "Loading inferences";
   unordered_map<string, vector<float>> pmid2mix;
   nodeIdx id;
@@ -138,6 +78,7 @@ int main(int argc, char ** argv){
   float tmp;
   fstream cloudFile(cloudPath, ios::in);
   fstream inferenceFile(inferencePath, ios::in);
+  // Note: one line of cloud file is equal to one line in inf
   while(cloudFile >> id && getline(inferenceFile, line)){
     stringstream ss(line);
     vector<float> mix;
@@ -149,63 +90,23 @@ int main(int argc, char ** argv){
     }
     mix /= sum; // normalize to prob func
     const string& pmid = labels[id];
-    pmid2mix[pmid] = move(mix);
+    pmid2mix[label2pmid(pmid)] = move(mix);
   }
   cloudFile.close();
   inferenceFile.close();
   vout << "  -- Done --" << endl;
 
 
-  //vout << "Determining papers per topic";
-  //vector<vector<float>> desiredMixes;
-  //// the best mixes for each best topic is just that topic
-  //for(size_t i = 0; i < topicModel.size(); ++i){
-    //vector<float> mix(topicModel.size(), 0.0f);
-    //mix[i] = 1;
-    //desiredMixes.emplace_back(move(mix));
-  //}
-
-  //for(size_t i = 1; i < topicPath.size()-1; ++i){
-    //for(size_t j = i+1; j < topicPath.size()-1; ++j){
-      //vector<float> mix(topicModel.size(), 0.0f);
-      //for(size_t x = i; x < j; ++x){
-        //mix[x] = 1;
-      //}
-      //desiredMixes.emplace_back(move(mix));
-    //}
-  //}
-  vout << "  -- Done --" << endl;
-
   vout << "Determining best papers for mixtures";
   vector<pQueue<string, float>> bestPapersPerMix(topicModel.size());
 
   for(auto& pmidMix : pmid2mix){
     for(size_t i = 0 ; i < topicModel.size(); ++i){
+      // push in negative score for lower priority (pops our based on lower)
       bestPapersPerMix[i].push(pmidMix.first, -pmidMix.second[i]);
     }
   }
   vout << "  -- Done --" << endl;
-
-  //vout << "Collecting Papers";
-  //unordered_set<string> selectPmid;
-  //for(pQueue<string, float> paperQ : bestPapersPerMix){
-    //for(size_t i = 0; i < min(paperQ.size(), numPerMix); ++i){
-      //selectPmid.insert(paperQ.pop().first);
-    //}
-  //}
-  //vout << "  -- Done --" << endl;
-
-  //vout << "Finding Abstract Text";
-  //list<string> abstracts;
-  //auto selectPmidFun = [&selectPmid](const string& line) -> bool {
-    //stringstream ss(line);
-    //string tmp;
-    //ss >> tmp;
-    //return selectPmid.find(tmp) != selectPmid.end();
-  //};
-
-  //auto get = [](const string& line) -> string { return line; };
-  //fastProcFile<string>(abstractPath, abstracts, get, selectPmidFun);
 
   fstream outFile(outPath, ios::out);
   for(size_t i = 0; i < bestPapersPerMix.size(); ++i){
@@ -216,7 +117,6 @@ int main(int argc, char ** argv){
     outFile << endl;
   }
   outFile.close();
-  // vout << "  -- Done --" << endl;
 
   return 0;
 }
